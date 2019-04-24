bd1599259d66271eb82da1ebffc910bf46f13e9b
Vladislav <zxyghostyxz@gmail.com>
2019-04-25 02:28:08 +0300

    update main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        int a = 10;
-        double b = 20;
-        boolean c = false;
-        calcualte(a,b);
-        System.out.println(c);
-    }
-
-    private static void calcualte(int a, double b) {
-//        System.out.println(((double) a) + b);
+        System.out.println("Hello");
     }
 }
